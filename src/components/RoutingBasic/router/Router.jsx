055a3f9d4e57247411aa6cc093ce5a82b47ac786
Sota <30281161+sota0121@users.noreply.github.com>
2022-06-13 02:08:40 +0900

    enabel useRoutes style

--- src/components/RoutingBasic/router/Router.jsx
+++ src/components/RoutingBasic/router/Router.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useRoutes } from "react-router-dom";
 
 import { Home } from "../Home";
 import { Page1 } from "../Page1";
@@ -7,19 +7,32 @@ import { Page1a } from "../Page1a";
 import { Page1b } from "../Page1b";
 import { NotFound } from "../NotFound";
 
+// See for useRoutes
+// https://reactrouter.com/docs/en/v6/hooks/use-routes
+// ---
+// See also this site
+// Official doc is not enough to understand useRoutes
+// https://qiita.com/jonakp/items/ce5cab870e9ccd58b46c
+
 export const Router = (props) => {
-  return (
-    <>
-      {/* Routing */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />}>
-          <Route path="a" element={<Page1a />} />
-          <Route path="b" element={<Page1b />} />
-        </Route>
-        <Route path="page2" element={<Page2 />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+  const routerElement = useRoutes([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "page1",
+      element: <Page1 />,
+      children: [
+        { path: "a", element: <Page1a /> },
+        { path: "b", element: <Page1b /> }
+      ]
+    },
+    {
+      path: "page2",
+      element: <Page2 />
+    },
+    { path: "*", element: <NotFound /> }
+  ]);
+  return routerElement;
 };
