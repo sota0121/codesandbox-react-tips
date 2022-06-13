2fc6c1281617b02247d2a3383b3d19e81d1f73ee
Sota <30281161+sota0121@users.noreply.github.com>
2022-06-13 12:32:40 +0900

    go to the page without clicking link

--- src/components/RoutingBasic/Page1.jsx
+++ src/components/RoutingBasic/Page1.jsx
@@ -1,13 +1,26 @@
-import { BrowserRouter, Link, Outlet, Routes, Route } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import { Page1a } from "./Page1a";
 import { Page1b } from "./Page1b";
 
+// See: what change on react-router-dom from v5 to v6 ?
+// https://dev.classmethod.jp/articles/react-router-5to6/
+// ---
+// 1. useHistory --> useNavigate
+
 export const Page1 = (props) => {
   console.log("Page1");
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/page1/a");
+  };
+
   return (
     <>
       <h2>Page1</h2>
+      <br />
+      <button onClick={handleClick}>Go to Page1a</button>
       <Outlet />
     </>
   );
